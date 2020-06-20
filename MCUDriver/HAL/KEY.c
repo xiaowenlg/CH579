c80a9274e1ea593c956c68d7c52d37b882988a9c
xiaowenlg <daiyu0@163.com>
2020-06-20 08:30:21 +0800

    创建完成...

--- MCUDriver/HAL/KEY.c
+++ MCUDriver/HAL/KEY.c
@@ -6,31 +6,26 @@
 * Description        : 
 *******************************************************************************/
 
-
-
 /******************************************************************************/
 /* 头文件包含 */
 #include "CH57x_common.h"
 #include "HAL.h"
 
-
 /**************************************************************************************************
  *                                        GLOBAL VARIABLES
  **************************************************************************************************/
 
-uint8 Hal_KeyIntEnable;           /* interrupt enable/disable flag */
+uint8 Hal_KeyIntEnable; /* interrupt enable/disable flag */
 
 // Registered keys task ID, initialized to NOT USED.
 static uint8 registeredKeysTaskID = TASK_NO_TASK;
-static uint8 halKeySavedKeys;     /* 保留按键最后的状态，用于查询是否有键值变化 */
-static uint8 KeyConfigFlag;		    /* 按键是否配置标志位 */
-
+static uint8 halKeySavedKeys; /* 保留按键最后的状态，用于查询是否有键值变化 */
+static uint8 KeyConfigFlag;   /* 按键是否配置标志位 */
 
 /**************************************************************************************************
  *                                        FUNCTIONS - Local
  **************************************************************************************************/
-static halKeyCBack_t pHalKeyProcessFunction;	    /* callback function */
-
+static halKeyCBack_t pHalKeyProcessFunction; /* callback function */
 
 /**************************************************************************************************
  * @fn      HAL_KeyInit
@@ -41,22 +36,22 @@ static halKeyCBack_t pHalKeyProcessFunction;	    /* callback function */
  *
  * @return  None
  **************************************************************************************************/
-void HAL_KeyInit( void )
+void HAL_KeyInit(void)
 {
   /* Initialize previous key to 0 */
   halKeySavedKeys = 0;
   /* Initialize callback function */
-  pHalKeyProcessFunction  = NULL;
+  pHalKeyProcessFunction = NULL;
   /* Start with key is not configured */
   KeyConfigFlag = FALSE;
   KEY1_DIR;
   KEY1_PU;
-  HalKeyConfig( HAL_KEY_INTERRUPT_DISABLE, HalKeyCallback );
+  HalKeyConfig(HAL_KEY_INTERRUPT_DISABLE, HalKeyCallback);
 }
 
-void HAL_KEY_RegisterForKeys( tmosTaskID id )
+void HAL_KEY_RegisterForKeys(tmosTaskID id)
 {
-	registeredKeysTaskID = id;
+  registeredKeysTaskID = id;
 }
 
 /**************************************************************************************************
@@ -69,21 +64,24 @@ void HAL_KEY_RegisterForKeys( tmosTaskID id )
  *
  * @return  None
  **************************************************************************************************/
-void HalKeyConfig (uint8 interruptEnable, halKeyCBack_t cback)
+void HalKeyConfig(uint8 interruptEnable, halKeyCBack_t cback)
 {
   /* Enable/Disable Interrupt or */
   Hal_KeyIntEnable = interruptEnable;
   /* Register the callback fucntion */
   pHalKeyProcessFunction = cback;
   /* Determine if interrupt is enable or not */
-  if (Hal_KeyIntEnable){
+  if (Hal_KeyIntEnable)
+  {
     /* Do this only after the hal_key is configured - to work with sleep stuff */
-    if (KeyConfigFlag == TRUE){
-      tmos_stop_task( halTaskID, HAL_KEY_EVENT );  /* Cancel polling if active */
+    if (KeyConfigFlag == TRUE)
+    {
+      tmos_stop_task(halTaskID, HAL_KEY_EVENT); /* Cancel polling if active */
     }
   }
-  else{    /* Interrupts NOT enabled */
-    tmos_start_task( halTaskID, HAL_KEY_EVENT, HAL_KEY_POLLING_VALUE);    /* Kick off polling */
+  else
+  {                                                                   /* Interrupts NOT enabled */
+    tmos_start_task(halTaskID, HAL_KEY_EVENT, HAL_KEY_POLLING_VALUE); /* Kick off polling */
   }
   /* Key now is configured */
   KeyConfigFlag = TRUE;
@@ -99,23 +97,26 @@ void HalKeyConfig (uint8 interruptEnable, halKeyCBack_t cback)
  *
  * @return  status
  *********************************************************************/
-uint8 OnBoard_SendKeys( uint8 keys, uint8 state )
+uint8 OnBoard_SendKeys(uint8 keys, uint8 state)
 {
   keyChange_t *msgPtr;
 
-  if ( registeredKeysTaskID != TASK_NO_TASK ){
+  if (registeredKeysTaskID != TASK_NO_TASK)
+  {
     // Send the address to the task
-    msgPtr = (keyChange_t *)tmos_msg_allocate( sizeof(keyChange_t) );
-    if ( msgPtr ){
+    msgPtr = (keyChange_t *)tmos_msg_allocate(sizeof(keyChange_t));
+    if (msgPtr)
+    {
       msgPtr->hdr.event = KEY_CHANGE;
       msgPtr->state = state;
       msgPtr->keys = keys;
-      tmos_msg_send( registeredKeysTaskID, (uint8 *)msgPtr );
+      tmos_msg_send(registeredKeysTaskID, (uint8 *)msgPtr);
     }
-    return ( SUCCESS );
+    return (SUCCESS);
   }
-  else{
-    return ( FAILURE );
+  else
+  {
+    return (FAILURE);
   }
 }
 
@@ -129,25 +130,29 @@ uint8 OnBoard_SendKeys( uint8 keys, uint8 state )
  *
  * @return  void
  *********************************************************************/
-void HalKeyCallback ( uint8 keys, uint8 state )
+void HalKeyCallback(uint8 keys, uint8 state)
 {
   (void)state;
-  if ( OnBoard_SendKeys( keys, state ) != SUCCESS ){
+  if (OnBoard_SendKeys(keys, state) != SUCCESS)
+  {
     // Process SW1 here
-    if ( keys & HAL_KEY_SW_1 ){  // Switch 1
-		}
+    if (keys & HAL_KEY_SW_1)
+    { // Switch 1
+    }
     // Process SW2 here
-    if ( keys & HAL_KEY_SW_2 ){  // Switch 2
+    if (keys & HAL_KEY_SW_2)
+    { // Switch 2
     }
     // Process SW3 here
-    if ( keys & HAL_KEY_SW_3 ){  // Switch 3
+    if (keys & HAL_KEY_SW_3)
+    { // Switch 3
     }
     // Process SW4 here
-    if ( keys & HAL_KEY_SW_4 ){  // Switch 4
+    if (keys & HAL_KEY_SW_4)
+    { // Switch 4
     }
   }
 }
-
 
 /**************************************************************************************************
  * @fn      HalKeyRead
@@ -158,25 +163,28 @@ void HalKeyCallback ( uint8 keys, uint8 state )
  *
  * @return  keys - current keys status
  **************************************************************************************************/
-uint8 HalKeyRead ( void )
+uint8 HalKeyRead(void)
 {
   uint8 keys = 0;
 
-  if (HAL_PUSH_BUTTON1()){ //读按键1
+  if (HAL_PUSH_BUTTON1())
+  { //读按键1
     keys |= HAL_KEY_SW_1;
   }
-  if (HAL_PUSH_BUTTON2()){ //读按键1
+  if (HAL_PUSH_BUTTON2())
+  { //读按键1
     keys |= HAL_KEY_SW_2;
   }
-  if (HAL_PUSH_BUTTON3()){ //读按键1
+  if (HAL_PUSH_BUTTON3())
+  { //读按键1
     keys |= HAL_KEY_SW_3;
   }
-  if (HAL_PUSH_BUTTON4()){ //读按键1
+  if (HAL_PUSH_BUTTON4())
+  { //读按键1
     keys |= HAL_KEY_SW_4;
   }
   return keys;
 }
-
 
 /**************************************************************************************************
  * @fn      HAL_KeyPoll
@@ -187,33 +195,39 @@ uint8 HalKeyRead ( void )
  *
  * @return  None
  **************************************************************************************************/
-void HAL_KeyPoll (void)
+void HAL_KeyPoll(void)
 {
   uint8 keys = 0;
 
-  if( HAL_PUSH_BUTTON1() ){
+  if (HAL_PUSH_BUTTON1())
+  {
     keys |= HAL_KEY_SW_1;
   }
-  if( HAL_PUSH_BUTTON2() ){
+  if (HAL_PUSH_BUTTON2())
+  {
     keys |= HAL_KEY_SW_2;
   }
-  if( HAL_PUSH_BUTTON3() ){
+  if (HAL_PUSH_BUTTON3())
+  {
     keys |= HAL_KEY_SW_3;
   }
-  if( HAL_PUSH_BUTTON4() ){
+  if (HAL_PUSH_BUTTON4())
+  {
     keys |= HAL_KEY_SW_4;
   }
-  if (!Hal_KeyIntEnable){	             /* 中断未使能 */
-    if(keys == halKeySavedKeys){         /* Exit - since no keys have changed */
+  if (!Hal_KeyIntEnable)
+  { /* 中断未使能 */
+    if (keys == halKeySavedKeys)
+    { /* Exit - since no keys have changed */
       return;
-		}
-		halKeySavedKeys = keys;	       /* Store the current keys for comparation next time */
+    }
+    halKeySavedKeys = keys; /* Store the current keys for comparation next time */
   }
   /* Invoke Callback if new keys were depressed */
-  if (keys && (pHalKeyProcessFunction)){
-    (pHalKeyProcessFunction) (keys, HAL_KEY_STATE_NORMAL);
+  if (keys && (pHalKeyProcessFunction))
+  {
+    (pHalKeyProcessFunction)(keys, HAL_KEY_STATE_NORMAL);
   }
 }
-
 
 /******************************** endfile @ key ******************************/
